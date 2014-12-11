bed03b56073e79217e547620f90e2bd07a5e3ce2
Domenic Murtari <domenic.murtari@gmail.com>
2014-12-11 11:25:57 -0700

    Readded start probabilities

--- robot.py
+++ robot.py
@@ -12,20 +12,34 @@ class Robot():
         with open(file) as robot_data:
             contents = robot_data.readlines()
 
+        starts = {}
+        moves = 0
+        previous_line = "."
+        transitions = [[0 for x in range(5)] for x in range(5)] 
+
         for line in contents:
-            readline = line.rstrip()
-            if readline == ".":
+            read_line = line.rstrip()
+            if read_line == ".":
+                previous_line = read_line
+                continue
+            if read_line == "..":
+                print "End of Test Data"
                 break
-            coordinate, color = readline.split(" ")
+
+            coordinate, color = read_line.split(" ")
+            moves += 1
 
             if coordinate not in self.states:
                 self.states = self.states + (coordinate,) 
             if color not in self.observations:
                 self.observations = self.observations + (color,)
 
-            start_probability = 1/float(len(self.states))
+            if coordinate not in starts:
+                starts[coordinate] = 1
+            else:
+                starts[coordinate] += 1
 
-            for coordinate in self.states:
-                self.start_probability[coordinate] = start_probability
+        for coordinate in self.states:
+                self.start_probability[coordinate] = starts[coordinate]/float(moves)
 
         print self.states, self.observations, self.start_probability
