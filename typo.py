61ee6c12b68dd347f29c7a214f33f37f07fd0942
Irakli Zhuzhunashvili <irakli.zhuzhunashvili1@gmail.com>
2014-12-11 13:23:45 -0700

    Added emission_probability

--- typo.py
+++ typo.py
@@ -27,6 +27,12 @@ class Typo():
         for i in range(26):
             self.transition_probability[chr(ord('a') + i)] = {"counter" : 0}
 
+        for i in range(26):
+            self.emission_probability[chr(ord('a') + i)] = {"counter" : 0}
+            for j in range(26):
+                self.emission_probability[chr(ord('a') + i)][chr(ord('a') + j)] = 0
+
+
         first_line = True
         for line in contents:
             read_line = line.rstrip()
@@ -44,6 +50,7 @@ class Typo():
                 start_counter += 1
                 first_line = False
                 previous_letter = letters[0]
+
             else:
                 if letters[0] not in self.transition_probability[previous_letter]:
                     self.transition_probability[previous_letter][letters[0]] = 1
@@ -53,18 +60,23 @@ class Typo():
                     self.transition_probability[previous_letter]["counter"] += 1
                 previous_letter = letters[0]
 
+            self.emission_probability[letters[0]][letters[1]] += 1
+            self.emission_probability[letters[0]]["counter"] += 1
+
         for i, times in enumerate(start_letters):
             self.start_probability[chr(ord('a') + i)] = (times + 1)/float(start_counter + 26)
 
         for key in self.transition_probability:
-            print self.transition_probability[key]
             for child_key in self.transition_probability[key]:
                 if child_key != "counter":
                     count = self.transition_probability[key][child_key] + 1
-                    print count
                     self.transition_probability[key][child_key] = count / float(self.transition_probability[key]["counter"] + 26)
 
-        print self.transition_probability
+        for key in self.emission_probability:
+            for child_key in self.emission_probability[key]:
+                if child_key != "counter":
+                    count = self.emission_probability[key][child_key] + 1
+                    self.emission_probability[key][child_key] = count / float(self.emission_probability[key]["counter"] + 26)
 
         #     coordinate, color = read_line.split(" ")
         #     moves += 1
